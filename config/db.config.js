d2f45297c9c2cfea19e22ec36ff59ee19478397f
Old <onelifedesigning@gmail.com>
2020-07-25 12:40:18 +0200

    remove porcces error

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = porcces.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tweet-hack';
+const MONGODB_URI = proccess.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tweet-hack';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
